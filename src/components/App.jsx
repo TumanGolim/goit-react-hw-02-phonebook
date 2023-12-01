c3846cf873ae09974ab750d0225b79cce4053abc
TumanGolim <130469755+TumanGolim@users.noreply.github.com>
2023-12-01 21:13:20 +0200

    task1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,58 @@
-export const App = () => {
+import React, { useState } from 'react';
+import { nanoid } from 'nanoid';
+
+const ContactForm = ({ contacts, setContacts }) => {
+  const [name, setName] = useState('');
+
+  const handleNameChange = event => {
+    setName(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const newContact = {
+      id: nanoid(),
+      name: name,
+    };
+    setContacts([...contacts, newContact]);
+    setName('');
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+        value={name}
+        onChange={handleNameChange}
+      />
+      <button type="submit">Add Contact</button>
+    </form>
+  );
+};
+
+const ContactList = ({ contacts }) => {
+  return (
+    <ul>
+      {contacts.map(contact => (
+        <li key={contact.id}>{contact.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+
+  return (
+    <div>
+      <ContactForm contacts={contacts} setContacts={setContacts} />
+      <ContactList contacts={contacts} />
     </div>
   );
 };
+
+export default App;
