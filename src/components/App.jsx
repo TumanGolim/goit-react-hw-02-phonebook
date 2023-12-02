504605db385eda7ca2336cdf1c3c5727d6df4381
TumanGolim <130469755+TumanGolim@users.noreply.github.com>
2023-12-02 15:59:21 +0200

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,102 @@
 import React, { useState } from 'react';
-import ContactForm from './ContactForm';
-import Filter from './Filter';
-import ContactList from './ContactList';
+import { nanoid } from 'nanoid';
+
+const ContactForm = ({ addContact }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleNameChange = event => {
+    setName(event.target.value);
+  };
+
+  const handleNumberChange = event => {
+    setNumber(event.target.value);
+  };
+
+  const handleAddContact = () => {
+    if (name.trim() === '' || number.trim() === '') {
+      alert('Name and number cannot be empty');
+      return;
+    }
+
+    const newContact = {
+      id: nanoid(),
+      name: name,
+      number: number,
+    };
+
+    addContact(newContact);
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <div>
+      <h3>Name</h3>
+      <input
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+        value={name}
+        onChange={handleNameChange}
+      />
+      <h3>Number</h3>
+      <input
+        type="tel"
+        name="number"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+        value={number}
+        onChange={handleNumberChange}
+      />
+      <div>
+        <button type="button" onClick={handleAddContact}>
+          Add Contact
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const Filter = ({ filter, handleFilterChange }) => {
+  return (
+    <div>
+      <div style={{ marginBottom: '8px' }}>
+        <label htmlFor="filterInput">Find contacts by name</label>
+      </div>
+      <div>
+        <input
+          type="text"
+          id="filterInput"
+          value={filter}
+          onChange={handleFilterChange}
+        />
+      </div>
+    </div>
+  );
+};
+
+const ContactList = ({ contacts, filter, deleteContact }) => {
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <ul>
+      {filteredContacts.map(contact => (
+        <li key={contact.id}>
+          {contact.name}: {contact.number}
+          <button type="button" onClick={() => deleteContact(contact.id)}>
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 const App = () => {
   const [contacts, setContacts] = useState([]);
